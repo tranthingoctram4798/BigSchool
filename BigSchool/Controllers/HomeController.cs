a9b3489040917c9888c91032fe7fc37e9a40df83
Administrator <Administrator@PC007>
2019-04-02 09:10:23 +0700

    Add project files.

---
+++ BigSchool/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upcomingCourses = _dbContext.Courses
+                .Include("Lecturer")
+                .Include("Category")
+                .Where(c => c.DateTime > DateTime.Now);
+            var viewModel = new CouresViewModel
+            {
+                UpcomingCourses = upcomingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
